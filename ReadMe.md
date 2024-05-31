7c3c857eb7ff1e517f6938cd2ad5c981eb408a48
Ismail Essabbagh <ismail.ess.98@gmail.com>
2024-05-31 15:38:45 +0100

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+# Advertica-V-ComingSoon
+ 
---
+++ js/main.js
@@ -0,0 +1,160 @@
+(function($) {
+
+    "use strict";
+    
+    var cfg = {
+        scrollDuration : 800, // smoothscroll duration
+        formActionURL: 'https://formspree.io/f/{your_form_id}' // Formspree form action URL
+    },
+
+    $WIN = $(window);
+
+    // Add the User Agent to the <html>
+    // will be used for IE10 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0))
+    var doc = document.documentElement;
+    doc.setAttribute('data-useragent', navigator.userAgent);
+
+    // svg fallback
+    if (!Modernizr.svg) {
+        $(".home-logo img").attr("src", "images/logo.png");
+    }
+
+
+    /* Preloader
+    * -------------------------------------------------- */
+    var ssPreloader = function() {
+        
+        $("html").addClass('ss-preload');
+
+        $WIN.on('load', function() {
+
+            // will first fade out the loading animation 
+            $("#loader").fadeOut("slow", function() {
+                // will fade out the whole DIV that covers the website.
+                $("#preloader").delay(300).fadeOut("slow");
+            }); 
+            
+            // for hero content animations 
+            $("html").removeClass('ss-preload');
+            $("html").addClass('ss-loaded');
+        
+        });
+    };
+
+
+    /* info toggle
+    * ------------------------------------------------------ */
+    var ssInfoToggle = function() {
+
+        //open/close lateral navigation
+        $('.info-toggle').on('click', function(event) {
+
+            event.preventDefault();
+            $('body').toggleClass('info-is-visible');
+
+        });
+
+    };
+
+
+    /* slick slider
+    * ------------------------------------------------------ */
+    var ssSlickSlider = function() {
+        
+        $('.home-slider').slick({
+            arrows: false,
+            dots: false,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            fade: true,
+            speed: 3000
+        });
+
+    };
+
+
+    /* placeholder plugin settings
+    * ------------------------------------------------------ */
+    var ssPlaceholder = function() {
+        $('input, textarea, select').placeholder();
+    };
+
+
+    /* final countdown
+    * ------------------------------------------------------ */
+    var ssFinalCountdown = function() {
+
+        var finalDate =  new Date("June 9, 2024 00:00:00").getTime();
+        //-date: "Mar 25 2021",
+
+        $('.home-content__clock').countdown(finalDate)
+        .on('update.countdown finish.countdown', function(event) {
+
+            var str = '<div class=\"top\"><div class=\"time days\">' +
+                      '%D <span>day%!D</span>' + 
+                      '</div></div>' +
+                      '<div class=\"time hours\">' +
+                      '%H <span>H</span></div>' +
+                      '<div class=\"time minutes\">' +
+                      '%M <span>M</span></div>' +
+                      '<div class=\"time seconds\">' +
+                      '%S <span>S</span></div>';
+
+            $(this)
+            .html(event.strftime(str));
+
+        });
+    };
+
+
+    /* Form Submission
+    * ------------------------------------------------------ */
+    var ssFormSubmission = function() {
+        var form = document.getElementById("my-form");
+        form.addEventListener("submit", handleSubmit);
+
+        async function handleSubmit(event) {
+            event.preventDefault();
+            var status = document.getElementById("my-form-status");
+            var data = new FormData(event.target);
+            fetch(cfg.formActionURL, {
+                method: form.method,
+                body: data,
+                headers: {
+                    'Accept': 'application/json'
+                }
+            }).then(response => {
+                if (response.ok) {
+                    status.innerHTML = "Thanks for your submission!";
+                    form.reset();
+                } else {
+                    response.json().then(data => {
+                        if (Object.hasOwn(data, 'errors')) {
+                            status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
+                        } else {
+                            status.innerHTML = "Oops! There was a problem submitting your form";
+                        }
+                    });
+                }
+            }).catch(error => {
+                status.innerHTML = "Oops! There was a problem submitting your form";
+            });
+        }
+    };
+
+
+    /* initialize
+    * ------------------------------------------------------ */
+    (function ssInit() {
+        
+        ssPreloader();
+        ssInfoToggle();
+        ssSlickSlider();
+        ssPlaceholder();
+        ssFinalCountdown();
+        ssFormSubmission();
+
+    })();
+
+
+})(jQuery);
